b7b967b778a25f2611cdef76f04d56af2bface3d
Moinuddin Muhammad Imtiaz Bhuiyan <85118978+imtiazzzz@users.noreply.github.com>
2023-05-14 13:17:02 +0600

    Update 1.asm

--- 1.asm
+++ 1.asm
@@ -1,10 +1,12 @@
 
-; You may customize this and other start-up templates; 
-; The location of this template is c:\emu8086\inc\0_com_template.txt
-
 org 100h
+MOV AX,0B800h;
+MOV DS,AX;
+MOV CL,'A';
+MOV CH,1101_1111b;
+MOV [BX],CX;
+RET;
 
-; add your code here
 
 ret
 
